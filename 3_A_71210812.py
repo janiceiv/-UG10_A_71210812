227f89b4a07e89e283d17aced7ca2280c87fd688
Janice <janice.ivena@ti.ukdw.ac.id>
2021-11-19 01:46:41 +0700

    3

---
+++ 3_A_71210812.py
@@ -0,0 +1,18 @@
+arah = input("Mendatar/Menurun?: ")
+
+if arah == "Mendatar":
+   int(input("Masukkan baris: "))
+   print("#####")
+    
+elif arah == "Menurun":
+   int(input("Masukkan baris: "))
+   print("*")
+   print("*")
+   print("*")
+   print("*")
+   print("*")
+   print("*")
+   print("*")
+    
+else: 
+    print("Pola tidak dikenali")
